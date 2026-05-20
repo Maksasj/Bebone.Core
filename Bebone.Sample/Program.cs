a8ec19dde7f576a4d50bf1ebe01d4d2123e85f97
Maksasj <radioboos@gmail.com>
2026-05-20 18:40:45 +0300

    Add Bebone.Sample project with Veldrid rendering demo
    Added Bebone.Sample as a new executable project targeting .NET 10.0 and referencing Bebone.Core. Implemented an Application class that initializes a window and renders a colored quad using Veldrid (Vulkan backend). Updated Bebone.Core.csproj with Veldrid and Newtonsoft.Json dependencies and added a Graphics folder. Registered Bebone.Sample in the solution file.

---
+++ Bebone.Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Bebone.Sample
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var app = new Application();
+
+            app.PreInit();
+            app.Init();
+            app.Load();
+            app.Run();
+            app.Unload();
+        }
+    }
+}
