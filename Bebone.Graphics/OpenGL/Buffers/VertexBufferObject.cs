2bfea815920012eedf05ba5cea777293160cc8b5
Maksasj <radioboos@gmail.com>
2026-05-23 01:08:49 +0300

    Add dispose guard and ObjectDisposedException checks to VBO, EBO, and VAO

--- Bebone.Graphics/OpenGL/Buffers/VertexBufferObject.cs
+++ Bebone.Graphics/OpenGL/Buffers/VertexBufferObject.cs
@@ -7,32 +7,53 @@ public class VertexBufferObject : IDisposable
 {
     private readonly IGLContext _gl;
     private readonly uint _handle;
-
+    private bool _disposed;
     public VertexBufferObject(IGLContext gl)
     {
         _gl = gl;
 
         _handle = _gl.GenBuffer();
+        _disposed = false;
     }
 
-    public void Bind() => _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _handle);
+    public void Bind()
+    {
+        ObjectDisposedException.ThrowIf(_disposed, this);
 
-    public void Unbind() => _gl.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
+        _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _handle);
+    }
+
+    public void Unbind()
+    {
+        ObjectDisposedException.ThrowIf(_disposed, this);
+
+        _gl.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
+    }
 
     public unsafe void BufferData<T>(T[] data) where T : unmanaged
     {
+        ObjectDisposedException.ThrowIf(_disposed, this);
+
         fixed (void* v = &data[0])
             _gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint)(data.Length * sizeof(T)), v, BufferUsageARB.StaticDraw);
     }
 
     public unsafe void BufferSubData<T>(T[] data) where T : unmanaged
     {
+        ObjectDisposedException.ThrowIf(_disposed, this);
+
         fixed (void* v = &data[0])
             _gl.BufferSubData(BufferTargetARB.ArrayBuffer, 0, (nuint)(data.Length * sizeof(T)), v);
     }
 
     public void Dispose()
     {
+        if (_disposed)
+            return;
+
         _gl.DeleteBuffer(_handle);
+        _disposed = true;
+
+        GC.SuppressFinalize(this);
     }
 }
