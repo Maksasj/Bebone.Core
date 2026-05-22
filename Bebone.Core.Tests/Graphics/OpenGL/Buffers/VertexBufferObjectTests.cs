ff58ef2d520617ba9c21270ca5b4ea57edd396b3
Maksasj <radioboos@gmail.com>
2026-05-22 19:17:19 +0300

    Added vertex buffer object constructor test

---
+++ Bebone.Core.Tests/Graphics/OpenGL/Buffers/VertexBufferObjectTests.cs
@@ -0,0 +1,25 @@
+﻿using Bebone.Graphics.OpenGL.Buffers;
+using Moq;
+using Silk.NET.OpenGL;
+
+namespace Bebone.Core.Tests.Graphics.OpenGL.Buffers;
+
+[TestFixture]
+public class VertexBufferObjectTests
+{
+    [Test]
+    public void Constructor_CallGenBufferOnce()
+    {
+        // Arrange
+        var mockGL = new Mock<GL>();
+        mockGL
+            .Setup(s => s.GenBuffer())
+            .Returns(123);
+
+        // Act
+        var buffer = new VertexBufferObject(mockGL.Object);
+
+        // Assert
+        mockGL.Verify(s => s.GenBuffer(), Times.Once);
+    }
+}
