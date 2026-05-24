f36ff7dc0bccca276de8c933254ffb51f13fdbe1
Maksasj <radioboos@gmail.com>
2026-05-24 11:00:04 +0300

    Apply code review suggestions and fix build

--- Bebone.Graphics/Renderer/IDrawTask.cs
+++ Bebone.Graphics/Renderer/IDrawTask.cs
@@ -1,7 +1,6 @@
-﻿namespace Bebone.Graphics.Renderer
+﻿namespace Bebone.Graphics.Renderer;
+
+public interface IDrawTask<T>
 {
-    public interface IDrawTask<T>
-    {
-        public void Execute(T data);
-    }
+    public void Execute(T data);
 }
