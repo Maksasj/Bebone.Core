63ed094e88892825394be455b7cc64afdedef7b9
Maksasj <radioboos@gmail.com>
2026-05-25 23:00:32 +0300

    Add CreateMesh<T> to IGraphicsFactory and OpenGLFactory

--- Bebone.Graphics/Abstractions/IGraphicsFactory.cs
+++ Bebone.Graphics/Abstractions/IGraphicsFactory.cs
@@ -1,7 +1,12 @@
-﻿namespace Bebone.Graphics.Abstractions;
+﻿using Bebone.Graphics.Abstractions.Mesh;
+
+namespace Bebone.Graphics.Abstractions;
 
 public interface IGraphicsFactory
 {
+    public IMesh<T> CreateMesh<T>(T[] vertices, uint[] indices) where T : unmanaged, IVertex;
+
     public IShaderProgram CreateShader(string vertexShaderSource, string fragmentShaderSource);
+
     public ITexture CreateTexture(int width, int height, byte[] data);
 }
