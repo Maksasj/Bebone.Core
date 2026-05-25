7182996e3985ea70a42dc74766bc6b6bfc545bca
Maksasj <radioboos@gmail.com>
2026-05-25 23:04:08 +0300

    Add IMeshGenerator interface with Quad and Cube mesh generators

---
+++ Bebone.Graphics/Utils/IMeshGenerator.cs
@@ -0,0 +1,8 @@
+﻿using Bebone.Graphics.Abstractions.Mesh;
+
+namespace Bebone.Graphics.Utils;
+
+public interface IMeshGenerator<T> where T : IVertex
+{
+    public IMesh<T> GenerateMesh(IMeshBuilder<T> builder);
+}
---
+++ Bebone.Graphics/Utils/QuadMeshGenerator .cs
@@ -0,0 +1,23 @@
+﻿using Bebone.Graphics.Abstractions.Mesh;
+using System.Numerics;
+
+namespace Bebone.Graphics.Utils;
+
+public class QuadMeshGenerator : IMeshGenerator<Vertex>
+{
+    public IMesh<Vertex> GenerateMesh(IMeshBuilder<Vertex> builder)
+    {
+        var v00 = new Vertex(new Vector3(-0.5f, 0.5f, 0.0f), Vector3.One, new Vector2(0.0f, 1.0f));
+        var v01 = new Vertex(new Vector3(-0.5f, -0.5f, 0.0f), Vector3.One, new Vector2(0.0f, 0.0f));
+        var v02 = new Vertex(new Vector3(0.5f, -0.5f, 0.0f), Vector3.One, new Vector2(1.0f, 0.0f));
+
+        var v10 = new Vertex(new Vector3(-0.5f, 0.5f, 0.0f), Vector3.One, new Vector2(0.0f, 1.0f));
+        var v11 = new Vertex(new Vector3(0.5f, -0.5f, 0.0f), Vector3.One, new Vector2(1.0f, 0.0f));
+        var v12 = new Vertex(new Vector3(0.5f, 0.5f, 0.0f), Vector3.One, new Vector2(1.0f, 1.0f));
+
+        builder.AddTriangle(v00, v01, v02);
+        builder.AddTriangle(v10, v11, v12);
+
+        return builder.Build();
+    }
+}
