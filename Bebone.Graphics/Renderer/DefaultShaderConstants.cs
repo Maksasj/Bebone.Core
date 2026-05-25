7281db0a9255d065ff845818bcdc2cd6af93f178
Maksasj <radioboos@gmail.com>
2026-05-25 22:52:45 +0300

    Extract default shader sources into DefaultShaderConstants

---
+++ Bebone.Graphics/Renderer/DefaultShaderConstants.cs
@@ -0,0 +1,36 @@
+﻿namespace Bebone.Graphics.Renderer;
+
+public static class DefaultShaderConstants
+{
+    public const string DefaultVertexShader = """
+        #version 330 core
+        layout(location = 0) in vec3 aPosition;
+        layout(location = 1) in vec3 aNormal;
+        layout(location = 2) in vec2 aTexCoords;
+
+        uniform mat4 cam;
+
+        out vec2 texCoords;
+        out vec3 normal;
+
+        void main()
+        {
+            gl_Position = cam * vec4(aPosition, 1.0);
+            texCoords = aTexCoords;
+            normal = aNormal;
+        }
+        """;
+
+    public const string DefaultFragmentShader = """
+        #version 330 core
+        in vec2 texCoords;
+        in vec3 normal;
+
+        out vec4 fragColor;
+
+        void main()
+        {
+            fragColor = vec4(normal * 0.5 + 0.5, 1.0);
+        }
+        """;
+}
--- Bebone.Graphics/Renderer/Renderer.cs
+++ Bebone.Graphics/Renderer/Renderer.cs
@@ -1,5 +1,4 @@
 ﻿using Bebone.Graphics.Abstractions;
-using Bebone.Graphics.Camera;
 using Bebone.Graphics.RenderGraph;
 using System.Drawing;
 
@@ -7,38 +6,6 @@ namespace Bebone.Graphics.Renderer;
 
 public class Renderer
 {
-    private const string DefaultVertexShader = """
-        #version 330 core
-        layout(location = 0) in vec3 aPosition;
-        layout(location = 1) in vec3 aNormal;
-        layout(location = 2) in vec2 aTexCoords;
-
-        uniform mat4 cam;
-
-        out vec2 texCoords;
-        out vec3 normal;
-
-        void main()
-        {
-            gl_Position = cam * vec4(aPosition, 1.0);
-            texCoords = aTexCoords;
-            normal = aNormal;
-        }
-        """;
-
-    private const string DefaultFragmentShader = """
-        #version 330 core
-        in vec2 texCoords;
-        in vec3 normal;
-
-        out vec4 fragColor;
-
-        void main()
-        {
-            fragColor = vec4(normal * 0.5 + 0.5, 1.0);
-        }
-        """;
-
     private readonly List<IDrawTask<int>> _mainPassTasks;
     private readonly List<IDrawTask<int>> _uiPassTasks;
 
@@ -53,7 +20,7 @@ public class Renderer
         _mainPassTasks = [];
         _uiPassTasks = [];
 
-        _shaderProgram = factory.CreateShader(DefaultVertexShader, DefaultFragmentShader);
+        _shaderProgram = factory.CreateShader(DefaultShaderConstants.DefaultVertexShader, DefaultShaderConstants.DefaultFragmentShader);
 
         MainPass = new RenderQueuePass(context, _shaderProgram, _mainPassTasks, enableDepthTest: true);
         UiPass = new RenderQueuePass(context, _shaderProgram, _uiPassTasks, enableDepthTest: false);
