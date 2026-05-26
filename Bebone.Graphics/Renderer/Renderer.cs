852605429edbf0767a6ff094f0b46fbbd853b0f6
Maksasj <radioboos@gmail.com>
2026-05-26 18:47:28 +0300

    Extract ClearPass and expose it as a public property on Renderer

--- Bebone.Graphics/Renderer/Renderer.cs
+++ Bebone.Graphics/Renderer/Renderer.cs
@@ -1,11 +1,11 @@
 ﻿using Bebone.Graphics.Abstractions;
 using Bebone.Graphics.RenderGraph;
-using System.Drawing;
 
 namespace Bebone.Graphics.Renderer;
 
 public class Renderer
 {
+    public ClearPass ClearPass { get; init; }
     public RenderQueuePass MainPass { get; init; }
     public RenderQueuePass UiPass { get; init; }
 
@@ -14,14 +14,16 @@ public class Renderer
     public Renderer(IGLContext context, IGraphicsFactory factory)
     {
         var shaderProgram = factory.CreateShader(DefaultShaderConstants.DefaultVertexShader, DefaultShaderConstants.DefaultFragmentShader);
+
+        ClearPass = new ClearPass(context);
         MainPass = new RenderQueuePass(context, shaderProgram, enableDepthTest: true);
         UiPass = new RenderQueuePass(context, shaderProgram, enableDepthTest: false);
 
-        _frameGraph = new FrameGraph();
-        _frameGraph.AddPass(CreateClearPass(context));
-        _frameGraph.AddPass(MainPass);
-        _frameGraph.AddPass(UiPass);
-        _frameGraph.Compile();
+        _frameGraph = new FrameGraphBuilder()
+            .AddPass(ClearPass)
+            .AddPass(MainPass)
+            .AddPass(UiPass)
+            .Compile();
     }
 
     public void Render()
@@ -29,16 +31,5 @@ public class Renderer
         var frameData = new FrameData(1920, 1080, 0);
 
         _frameGraph.Execute(frameData);
-    }
-
-    private static RenderTask<int> CreateClearPass(IGLContext context)
-    {
-        return new RenderTask<int>(
-        _ => 0,
-        (_, _) =>
-        {
-            context.ClearColor(Color.FromArgb(255, 135, 206, 235));
-            context.ClearBuffers();
-        });
     }
 }
